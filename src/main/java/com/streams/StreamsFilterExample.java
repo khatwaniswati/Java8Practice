aa9bc95de3e3a810cbc81c42adfbff4727c9008e
Amit Khatwani <swati22khatwani@gmail.com>
2020-10-02 07:34:40 +0530

    Streams

---
+++ src/main/java/com/streams/StreamsFilterExample.java
@@ -0,0 +1,29 @@
+package com.streams;
+
+import com.data.Student;
+import com.data.StudentDataBase;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+public class StreamsFilterExample {
+
+    public static List<Student> filterStudents() {
+
+        List<Student> filteredStudentList = StudentDataBase.getAllStudents()
+                .stream()
+                .filter(student -> student.getGpa() >= 3.9)
+                .filter(student -> student.getGender().equals("female"))
+                .collect(toList());
+
+        return filteredStudentList;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Filtered Students : " + filterStudents());
+
+    }
+}
