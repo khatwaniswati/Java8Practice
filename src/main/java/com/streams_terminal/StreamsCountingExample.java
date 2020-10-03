bbf14a57b9d39f7e621f474bcc1b1f03a0674a42
Amit Khatwani <swati22khatwani@gmail.com>
2020-10-03 22:44:24 +0530

    Stream-TerminalOperations

---
+++ src/main/java/com/streams_terminal/StreamsCountingExample.java
@@ -0,0 +1,22 @@
+package com.streams_terminal;
+
+import com.data.StudentDataBase;
+
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.counting;
+
+public class StreamsCountingExample {
+
+    public static long count(){
+       return  StudentDataBase.getAllStudents()
+                .stream()
+                .filter(student -> student.getGpa()>=3.9)
+                .collect(counting());
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(count());
+    }
+}
